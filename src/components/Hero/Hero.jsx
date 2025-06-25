7c27080f796ff9a5b4a00cade83473788523514e
Hakeem Kasali <keem.kasali@gmail.com>
2025-06-25 11:41:57 +0100

    update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
         <div className={styles.content}>
             <h1 className={styles.title}> Hi, i'm Hakeem</h1>
             <p className={styles.description}>
-            I am a computing graduate passionate about AI, software development, and cloud systems currently based in London. My technical expertise spans includes frontend (React, Vue, JavaScript, HTML/CSS), backend (Node.js, Java, SQL), and mobile development (Android, Xamarin). 
+            I am a computing graduate passionate about AI, software development, and cloud systems currently based in London. My technical expertise spans across frontend (React, Vue, JavaScript, HTML/CSS), backend (Node.js, Java, SQL), and mobile development (Android, Xamarin). 
             I’ve built projects like an AI-powered symptom checker and a natural language healthcare virtual assistant, combining real-world impact with cutting-edge technology. Open to internships and collaborations — let's build something awesome!
             </p>
             <a href='mailto:keem.kasali@gmail.com' className={styles.contactBtn}>
